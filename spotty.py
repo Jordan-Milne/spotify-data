3d7e834cb15b306e6ae59e29530f65e6b04e03f1
Jordan <jordan.milne2@gmail.com>
2020-05-15 15:58:56 -0400

    base model

--- spotty.py
+++ spotty.py
@@ -1,54 +1,106 @@
 import os
 
-import spotipy
-import spotipy.util as util
-
-import pandas as pd
-
 from dotenv import load_dotenv, find_dotenv
+import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Loading API keys
 load_dotenv(find_dotenv())
 
 cid = os.environ['CLIENT_ID']
 secret = os.environ['CLIENT_SECRET']
 
-token = util.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-cache_token = token.get_access_token()
-sp = spotipy.Spotify(cache_token)
-
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
-
+# Authenticating API Credentials
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-
-def analyze_playlist(creator, playlist_id):
+# Function to make dataframe from playlist
+def analyze_playlist(creator, playlist_id,person):
 
     # Create empty dataframe
-    playlist_features_list = ["artist","album","track_name",  "track_id","danceability","energy","key","loudness","mode", "speechiness","instrumentalness","liveness","valence","tempo", "duration_ms","time_signature"]
+    column_list = ["artist","album","track_name",  "track_id","danceability","energy","key","loudness","mode", "speechiness","instrumentalness","liveness","valence","tempo", "duration_ms","time_signature"]
 
-    playlist_df = pd.DataFrame(columns = playlist_features_list)
+    main_df = pd.DataFrame(columns = column_list)
 
-    # Loop through every track in the playlist, extract features and append the features to the playlist df
+    # Looping through playlist and appending features
 
     playlist = sp.user_playlist_tracks(creator, playlist_id)["items"]
     for track in playlist:        # Create empty dict
-        playlist_features = {}        # Get metadata
-        playlist_features["artist"] = track["track"]["album"]["artists"][0]["name"]
-        playlist_features["album"] = track["track"]["album"]["name"]
-        playlist_features["track_name"] = track["track"]["name"]
-        playlist_features["track_id"] = track["track"]["id"]
+        features = {}        # Get metadata
+        features["artist"] = track["track"]["album"]["artists"][0]["name"]
+        features["album"] = track["track"]["album"]["name"]
+        features["track_name"] = track["track"]["name"]
+        features["track_id"] = track["track"]["id"]
 
-        # Get audio features
-        audio_features = sp.audio_features(playlist_features["track_id"])[0]
-        for feature in playlist_features_list[4:]:
-            playlist_features[feature] = audio_features[feature]
+        # collecting meta data (audio features)
+        audio_features = sp.audio_features(features["track_id"])[0]
+        for feature in column_list[4:]:
+            features[feature] = audio_features[feature]
 
-        # Concat the dfs
-        track_df = pd.DataFrame(playlist_features, index = [0])
-        playlist_df = pd.concat([playlist_df, track_df], ignore_index = True)
+        # merge
+        track_df = pd.DataFrame(features, index = [0])
+        main_df = pd.concat([main_df, track_df], ignore_index = True)
+    main_df['person'] = person
 
-    return playlist_df
+    return main_df
 
-analyze_playlist("spotify", "37i9dQZF1DX5IDTimEWoTd")
+# Loading my personal playlist and my gf's personal playlists (top songs for 2018 & 2019 curated by spotify)
+play_list1_id = os.environ['pl1_id']
+play_list2_id = os.environ['pl2_id']
+play_list2_id = os.environ['pl3_id']
+play_list4_id = os.environ['pl4_id']
+
+df1 = analyze_playlist("spotify", play_list1_id,'jordan')
+df2 = analyze_playlist("spotify", play_list2_id,'kathryn')
+df3 = analyze_playlist("spotify", play_list3_id,'kathryn')
+df4 = analyze_playlist("spotify", play_list4_id,'jordan')
+
+# merge
+df = pd.concat([df1, df2,df3,df4], ignore_index = True)
+
+from sklearn.model_selection import train_test_split
+from sklearn_pandas import DataFrameMapper
+from sklearn.preprocessing import StandardScaler
+
+target = 'person'
+y = df[target]
+X = df.drop(target, axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+df.head()
+
+# DataFrame Mapper
+mapper = DataFrameMapper([
+     (['danceability'], StandardScaler()),
+     (['energy'], [StandardScaler()]),
+     (['key'], [StandardScaler()]),
+     (['loudness'],  [StandardScaler()]),
+     (['mode'], StandardScaler()),
+     (['speechiness'], StandardScaler()),
+     (['instrumentalness'], StandardScaler()),
+     (['liveness'],  StandardScaler()),
+     (['valence'], StandardScaler()),
+     (['tempo'],  StandardScaler()),
+     (['duration_ms'],  StandardScaler()),
+     ], df_out=True)
+
+Z_train = mapper.fit_transform(X_train)
+Z_test = mapper.transform(X_test)
+
+from sklearn.ensemble import RandomForestClassifier
+
+# Base Model
+model = RandomForestClassifier()
+model.fit(Z_train,y_train)
+model.score(Z_train,y_train)
+model.score(Z_test,y_test)
+model.predict(Z_test[1])
+
+# # personal testing
+# yhat = model.predict(Z_test)
+#
+#
+# pd.DataFrame({
+#     'y_true': y_test,
+#     'y_hat': yhat
+# })
